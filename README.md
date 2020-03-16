a07ed58580a06018aed6b71cf4955ed6daf565ef
TickTackk <me@ticktackk.io>
2020-03-16 22:39:20 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Report Reasons For XenForo 2.1.7+
+
+This add-on allows creating predefined report reasons.
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE](LICENSE.md) file for details
---
+++ Setup.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace TickTackk\ReportReasons;
+
+use XF\AddOn\AbstractSetup;
+use XF\AddOn\StepRunnerInstallTrait;
+use XF\AddOn\StepRunnerUninstallTrait;
+use XF\AddOn\StepRunnerUpgradeTrait;
+
+/**
+ * Class Setup
+ *
+ * @package TickTackk\ReportReasons
+ */
+class Setup extends AbstractSetup
+{
+	use StepRunnerInstallTrait;
+	use StepRunnerUpgradeTrait;
+	use StepRunnerUninstallTrait;
+}
